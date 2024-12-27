50022bb5ab18e0523d138f282692def5e7660265
Rahul Sharma <rs5802@gmail.com>
2024-12-27 23:42:38 +0530

    get and set method for Linked List implemented

--- LinkedList/index.js
+++ LinkedList/index.js
@@ -72,8 +72,28 @@ class LinkedList {
     }
   }
 
+  get(index) {
+    if(index >= this.length || index < 0) {
+        return undefined;
+    }else {
+        let temp = this.head;
+        for(let i = 0; i < index; i++) {
+            temp = temp.next
+        }
+        return temp;
+    }
+  }
+
+  set(index, number) {
+    let temp = this.get(index);
+    if(temp) {
+        temp.value = number
+        return true
+    }
+    return false;
+  }
+
   insert(index, number) {
-    console.log(index, " =============== ", this.length);
     if (index > this.length - 1) {
       return "Out of bound error";
     } else if (index === 0) {
@@ -99,11 +119,10 @@ class LinkedList {
 }
 
 const myLinkedList = new LinkedList(4);
-myLinkedList.push(5);
-myLinkedList.push(7);
-console.log(JSON.stringify(myLinkedList, null, 2));
+// myLinkedList.push(5);
+// myLinkedList.push(7);
 // console.log("### POP ###");
-// console.log(myLinkedList.pop());
+console.log(myLinkedList.pop());
 // console.log("### UNSHIFT ###");
 // myLinkedList.unshift(11);
 // console.log("### SHIFT ###");
@@ -111,8 +130,10 @@ console.log(JSON.stringify(myLinkedList, null, 2));
 // myLinkedList.shift();
 // console.log(myLinkedList.shift());
 // console.log(JSON.stringify(myLinkedList, null, 2));
-myLinkedList.insert(0, 11);
-myLinkedList.insert(3, 13);
-myLinkedList.insert(1, 19);
-console.log(JSON.stringify(myLinkedList.insert(5, 19), null, 2));
-console.log(JSON.stringify(myLinkedList.insert(9, 15), null, 2));
+// myLinkedList.insert(0, 11);
+// myLinkedList.insert(3, 13);
+// myLinkedList.insert(1, 19);
+console.log("### GET ###");
+// console.log(JSON.stringify(myLinkedList, null, 2));
+console.log(myLinkedList.get(1));
+// console.log(myLinkedList.get(10));
